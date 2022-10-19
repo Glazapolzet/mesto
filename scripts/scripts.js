22a70731b37f102de2a112cbe1e0081742d872bf
Glazapolzet <VypZlol@yandex.ru>
2022-10-19 20:40:50 +0300

    исправления в соответствии с комментариями ревьюера

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,60 +5,38 @@ const cardsList = page.querySelector('.cards__list');
 const profile = page.querySelector('.profile');
 const pictureModal = page.querySelector('.picture-modal');
 
-const editProfilePopup = page.querySelector('.popup_use_edit-profile');
-const addPlacePopup = page.querySelector('.popup_use_add-place');
+const popupEditProfile = page.querySelector('.popup_use_edit-profile');
+const popupAddPlace = page.querySelector('.popup_use_add-place');
 
-const editUserButton = profile.querySelector('.profile__edit-user-button');
-const addPlaceButton = profile.querySelector('.profile__add-place-button');
+const buttonEditUser = profile.querySelector('.profile__edit-user-button');
+const buttonAddPlace = profile.querySelector('.profile__add-place-button');
 
 const userName = profile.querySelector('.profile__name');
 const userJob = profile.querySelector('.profile__job');
 
-const editProfilePopupForm = editProfilePopup.querySelector('.popup__form');
-const inputUserName = editProfilePopup.querySelector('.popup__input_content_user-name');
-const inputUserJob = editProfilePopup.querySelector('.popup__input_content_user-job');
+const popupFormEditProfile = popupEditProfile.querySelector('.popup__form');
+const inputUserName = popupEditProfile.querySelector('.popup__input_content_user-name');
+const inputUserJob = popupEditProfile.querySelector('.popup__input_content_user-job');
 
-const addPlacePopupForm = addPlacePopup.querySelector('.popup__form');
-const inputPlaceTitle = addPlacePopup.querySelector('.popup__input_content_place-title');
-const inputPlaceLink = addPlacePopup.querySelector('.popup__input_content_place-link');
+const popupFormAddPlace = popupAddPlace.querySelector('.popup__form');
+const inputPlaceTitle = popupAddPlace.querySelector('.popup__input_content_place-title');
+const inputPlaceLink = popupAddPlace.querySelector('.popup__input_content_place-link');
 
 const picture = pictureModal.querySelector('.picture-modal__picture');
 const pictureCaption = pictureModal.querySelector('.picture-modal__caption');
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
-function changePopupVisibility (popupName) {
-  popupName.classList.toggle('popup_opened');
+function showPopup (popupName) {
+  popupName.classList.add('popup_opened');
+}
+
+function closePopup (popupName) {
+  popupName.classList.remove('popup_opened');
 }
 
 function closeButtonsAddHandler () {
-  const closeButtons = page.querySelectorAll('.popup__close-button');
-  closeButtons.forEach(closeButton => closeButton.addEventListener('click', () => changePopupVisibility(closeButton.closest('.popup'))));
+  const buttonsClose = page.querySelectorAll('.popup__close-button');
+  buttonsClose.forEach(buttonClose => buttonClose.addEventListener('click', () => closePopup(buttonClose.closest('.popup'))));
 }
 
 function editProfilePopupFormSubmitHandler (evt) {
@@ -67,7 +45,7 @@ function editProfilePopupFormSubmitHandler (evt) {
   userName.textContent = inputUserName.value;
   userJob.textContent = inputUserJob.value;
 
-  changePopupVisibility(editProfilePopup);
+  closePopup(popupEditProfile);
 }
 
 function addPlacePopupFormSubmitHandler (evt) {
@@ -79,10 +57,9 @@ function addPlacePopupFormSubmitHandler (evt) {
 
   cardsList.prepend(createCard(newPlace));
 
-  changePopupVisibility(addPlacePopup);
+  closePopup(popupAddPlace);
   
-  inputPlaceTitle.value = '';
-  inputPlaceLink.value = '';
+  popupFormAddPlace.reset();
 }
 
 function createCard (item) {
@@ -95,32 +72,31 @@ function createCard (item) {
 
   cardImage.src = item.link;
   cardImage.alt = item.name;
-
   cardTitle.textContent = item.name;
 
   likeButton.addEventListener('click', evt => evt.target.classList.toggle('cards__like-button_active'));
   trashButton.addEventListener('click', evt => evt.target.parentElement.remove());
   cardImage.addEventListener('click', () => {
     picture.src = cardImage.src;
+    picture.alt = cardImage.alt;
     pictureCaption.textContent = cardTitle.textContent;
     
-    changePopupVisibility(pictureModal);
+    showPopup(pictureModal);
   })
   
   return card;
 }
 
-
 initialCards.forEach(item => cardsList.append(createCard(item)));
 closeButtonsAddHandler();
 
-editUserButton.addEventListener('click', () => {
+buttonEditUser.addEventListener('click', () => {
   inputUserName.value = userName.textContent;
   inputUserJob.value = userJob.textContent;
 
-  changePopupVisibility(editProfilePopup);
+  showPopup(popupEditProfile);
 });
-editProfilePopupForm.addEventListener('submit', editProfilePopupFormSubmitHandler);
+popupFormEditProfile.addEventListener('submit', editProfilePopupFormSubmitHandler);
 
-addPlaceButton.addEventListener('click', () => changePopupVisibility(addPlacePopup));
-addPlacePopupForm.addEventListener('submit', addPlacePopupFormSubmitHandler);
+buttonAddPlace.addEventListener('click', () => showPopup(popupAddPlace));
+popupFormAddPlace.addEventListener('submit', addPlacePopupFormSubmitHandler);
