8182020617be5c04628f1df05cc4f97975653685
Glazapolzet <VypZlol@yandex.ru>
2023-02-10 02:58:49 +0300

    fix: файловая структура

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import '../styles/pages/index.css';
+import './index.css';
 
-import {initialCards, config} from './constants.js';
-import Card from './Card.js';
-import PopupWithForm from "./PopupWithForm.js";
-import PopupWithImage from "./PopupWithImage.js";
-import Section from "./Section.js";
-import UserInfo from "./UserInfo.js";
-import FormValidator from "./FormValidator.js";
+import {initialCards, config} from '../components/constants.js';
+import Card from '../components/Card.js';
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import Section from "../components/Section.js";
+import UserInfo from "../components/UserInfo.js";
+import FormValidator from "../components/FormValidator.js";
 
 const page = document.querySelector('.page');
 
